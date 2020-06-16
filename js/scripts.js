66cc132d2663f6d1ba28b2a23e9ef71254fdd099
erdkok <erdkok@gmail.com>
2020-06-16 03:46:57 +0200

    Implemented js
    js for menu scroll and active menu etc

--- js/scripts.js
+++ js/scripts.js
@@ -25,20 +25,9 @@ win.scroll(function(e){
     });
 });
 
-$(document).ready(function(){
-		  $("a").on('click', function(event) {
-		
-			if (this.hash !== "") {
-			  event.preventDefault();
-		
-			  var hash = this.hash;
-		
-			  $('html, body').animate({
-				scrollTop: $(hash).offset().top - 90
-			  }, function(){
-		   
-				window.location.hash = hash;
-			  });
-			} 
-		  });
-		});
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top - 90}, 500);
+    });
+});
